62e3529eb29c65888f0d2df1759eac0e9c5716d6
yesiam <yesiam@YESIAMLAPTOP>
2022-11-07 21:48:48 +0300

    Добавьте файлы проекта.

---
+++ Самостоятельная работа 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число больше нуля");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            int p = 2 * n - 1;
+            for (int i=1; i<=n;i++)
+            {
+                s = i*i;
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
